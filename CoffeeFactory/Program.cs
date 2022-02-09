a12038c208658bdd26a21b3a28c91b6c8619685a
Philipp Dreher <dreher.philipp@googlemail.com>
2022-02-09 14:22:46 +0100

    created basics from templates

---
+++ CoffeeFactory/Program.cs
@@ -0,0 +1,10 @@
+using CoffeeFactory;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
